9d853ca18cccb76d87777d62ccba4435b99af3d8
Elya-vol13 <145384328+Elya-vol13@users.noreply.github.com>
2023-12-06 19:15:04 +0500

    play.js

--- play.js
+++ play.js
@@ -6,38 +6,44 @@ function getRandomInt(min, max) {
 
 function startGame() {
     var sticksCount = document.getElementById("sticksCount").value;
-    var sticksContainer = document.getElementById("sticksContainer");
-    sticksContainer.innerHTML = '';
-    for (var i = 0; i < sticksCount; i++) {
-        var randomStickNumber = getRandomInt(1, 4);
-        var stickImage;
-        switch (randomStickNumber) {
-          case 1:
-            var stick = document.createElement("img");
-            stick.src = "1.png";
-            stick.classList.add("stick");
-            sticksContainer.appendChild(stick);
-            break
-          case 2:
-            var stick = document.createElement("img");
-            stick.src = "2.png";
-            stick.classList.add("stick");
-            sticksContainer.appendChild(stick);
-            break
-          case 3:
-            var stick = document.createElement("img");
-            stick.src = "3.png";
-            stick.classList.add("stick");
-            sticksContainer.appendChild(stick);
-            break
-          case 4:
-            var stick = document.createElement("img");
-            stick.src = "4.png";
-            stick.classList.add("stick");
-            sticksContainer.appendChild(stick);
-            break
+    if (sticksCount > 25 | sticksCount < 20){
+        alert("Можно выбрать только от 20 до 25");
+    }
+    else{
+        var sticksContainer = document.getElementById("sticksContainer");
+        sticksContainer.innerHTML = '';
+        for (var i = 0; i < sticksCount; i++) {
+            var randomStickNumber = getRandomInt(1, 4);
+            var stickImage;
+            switch (randomStickNumber) {
+              case 1:
+                var stick = document.createElement("img");
+                stick.src = "1.png";
+                stick.classList.add("stick");
+                sticksContainer.appendChild(stick);
+                break
+              case 2:
+                var stick = document.createElement("img");
+                stick.src = "2.png";
+                stick.classList.add("stick");
+                sticksContainer.appendChild(stick);
+                break
+              case 3:
+                var stick = document.createElement("img");
+                stick.src = "3.png";
+                stick.classList.add("stick");
+                sticksContainer.appendChild(stick);
+                break
+              case 4:
+                var stick = document.createElement("img");
+                stick.src = "4.png";
+                stick.classList.add("stick");
+                sticksContainer.appendChild(stick);
+                break
 
       }
+    }
+    
     }
     
     document.getElementById("playerForm").style.display = "block";
@@ -47,31 +53,35 @@ function startGame() {
 function makeMove() {
     var playerInput = document.getElementById("playerInput").value;
     var sticks = document.getElementsByClassName("stick");
-    
-    var count = 0;
-    for (var i = 0; i < playerInput; i++) {
-        if (sticks.length > 0) {
-            sticks[sticks.length - 1].remove();
-            count++;
-        }
+    if (playerInput > 3){
+        alert("Можно забрать только 1, 2 или 3 палочки!")
     }
-    
-    if (sticks.length === 0) {
-        document.getElementById("message").innerHTML = "<span style='font-size: 42px; text-shadow: 0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px #0fa;'>" + "Поздравляю! Вы выйграли!"; + "</span>" 
-        document.getElementById("playerForm").style.display = "none";
-    } else {
-        var message = "Вы забрали " + count + " палоч";
-        if (count === 1) {
-            message += "ку";
+    else{
+        var count = 0;
+        for (var i = 0; i < playerInput; i++) {
+            if (sticks.length > 0) {
+                sticks[sticks.length - 1].remove();
+                count++;
+            }
         }
-        if (count > 1) {
-            message += "ки";
+        
+        if (sticks.length === 0) {
+            document.getElementById("message").innerHTML = "Поздравляю! Вы выйграли!";
+            document.getElementById("playerForm").style.display = "none";
+        } else {
+            var message = "Вы забрали " + count + " палоч";
+            if (count === 1) {
+                message += "ку";
+            }
+            if (count > 1) {
+                message += "ки";
+            }
+            if (count === 0){
+              message = "Вы не забрали палочки! Заберите палочки!"
+            }
+            document.getElementById("message").innerHTML =  message
         }
-        if (count === 0){
-          message = "Вы не забрали палочки! Заберите палочки!"
-        }
-        document.getElementById("message").innerHTML =  message
+        
+        document.getElementById("playerInput").value = "";
     }
-    
-    document.getElementById("playerInput").value = "";
 }
